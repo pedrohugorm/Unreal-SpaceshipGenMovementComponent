e24207a86b8d4569f2fbfba645f4ba65385c76d9
Pedro Matos <pedrohugorm@gmail.com>
2022-05-08 14:26:03 -0500

    Added Source

---
+++ SpaceshipPlugin/Source/SpaceshipPlugin/Public/SpaceshipPluginGameModeBase.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "SpaceshipPluginGameModeBase.generated.h"
+
+UCLASS()
+class SPACESHIPPLUGIN_API ASpaceshipPluginGameModeBase : public AGameMode
+{
+	GENERATED_BODY()
+	
+};
---
+++ SpaceshipPlugin/Source/SpaceshipPlugin/SpaceshipPlugin.Build.cs
@@ -0,0 +1,30 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class SpaceshipPlugin : ModuleRules
+{
+	public SpaceshipPlugin(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new[]
+		{
+			"Core", 
+			"CoreUObject", 
+			"Engine", 
+			"InputCore", 
+			"GeneralMovement"
+		});
+
+		PrivateDependencyModuleNames.AddRange(new string[] { });
+
+		// Uncomment if you are using Slate UI
+		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
+		// Uncomment if you are using online features
+		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+	}
+}
